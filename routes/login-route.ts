ca5d42114c20382a2e7be92741b39d1f8008060d
Lukas Singer <lukas.singer@outlook.com>
2022-01-13 20:50:21 +0100

    Create login route function
    -Add AuthResponse interface analogue to Angular application
    -Add LoginRequest interface analogue to Angular application

---
+++ routes/login-route.ts
@@ -0,0 +1,22 @@
+import {Express} from 'express';
+
+import {AuthResponse} from '../interfaces/auth-response';
+import {LoginRequest} from '../interfaces/login-request';
+
+export const login = (server: Express, url: string) => {
+  return server.get(url, (req, res) => {
+    const request: LoginRequest | any = req.params;
+    //TODO: check if user exists if it does statusCode 200 else 404 not found
+    if(false){
+      const response: AuthResponse = {statusCode: 200, message: 'LOGIN_SUCCESSFUL'};
+      res.send({
+        response,
+      });
+    }else{
+      const response: AuthResponse = {statusCode: 404, message: 'USER_OR_PASSWORD_INVALID'};
+      res.send({
+        response,
+      })
+    }
+  });
+}
