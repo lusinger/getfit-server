648b34b58de43a4e484a8c6c2ee1d35af98325eb
Lukas Singer <lukas.singer@outlook.com>
2022-01-13 18:23:39 +0100

    Create functions to sign and validate jwt tokens
    -Get private key from .key file
    -Get expiration date from .env file using dotenv

---
+++ modules/jwt.ts
@@ -0,0 +1,33 @@
+import jwt from 'jsonwebtoken';
+import fs from 'fs/promises';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const getPrivateKey = async() => {
+  const key = await fs.readFile('./keys/private-key.key');
+  return key;
+};
+
+const getExpTime = (): number => {
+  return process.env.JWT_EXP ? parseInt(process.env.JWT_EXP) : 120
+};
+
+const signSessionKey = async(payload: object): Promise<string> => {
+  const key = await getPrivateKey();
+  const token = jwt.sign(payload, key, {algorithm: 'RS256', expiresIn: getExpTime()});
+  return token;
+};
+
+const validateSessionToken = async(token: string) => {
+  const key = await getPrivateKey();
+  const response = jwt.verify(token, key);
+  console.log(response);
+};
+
+/* const test = async() => {
+  const token = await signSessionKey({mail: 'lukas.singer@outlook.com'});
+  console.log(token);
+  validateSessionToken(token);
+}
+
+test(); */
