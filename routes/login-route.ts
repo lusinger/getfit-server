81c87653d2c013255380eea424bd8bde655b32bd
Lukas Singer <lukas.singer@outlook.com>
2022-01-14 07:03:31 +0100

    Add todo point

--- routes/login-route.ts
+++ routes/login-route.ts
@@ -7,6 +7,7 @@ export const login = (server: Express, url: string) => {
   return server.get(url, (req, res) => {
     const request: LoginRequest | any = req.params;
     //TODO: check if user exists if it does statusCode 200 else 404 not found
+    //TODO: if the user exists and credentials match send an auth token to the client
     if(false){
       const response: AuthResponse = {statusCode: 200, message: 'LOGIN_SUCCESSFUL'};
       res.send({
