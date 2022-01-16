6ab0a70f2f0af823d41af8bb62b0d2fc48553599
Lukas Singer <lukas.singer@outlook.com>
2022-01-16 09:50:52 +0100

    Create register route
    -Return 201 if user is created
    -Return 409 if user already exists in databse

---
+++ routes/register-route.ts
@@ -0,0 +1,28 @@
+import { Express } from "express";
+
+import { encryptPw } from "../modules/pw-encription";
+import { registerUser } from "../modules/database/user-queries";
+
+import { RegisterRequest } from "../interfaces/register-request";
+import { AuthResponse } from "../interfaces/auth-response";
+
+export const register = async(server: Express, url: string): Promise<Express> => {
+  return server.post(url, async(req, res) => {
+    const request = req.body as RegisterRequest;
+
+    const hashedPw = await encryptPw(request.password);
+    const dbResponse = await registerUser(request, hashedPw);
+
+    if(true){
+      const response: AuthResponse = {statusCode: 201, message: 'USER_CREATED'};
+      res.send(
+        response,
+      );
+    }else{
+      const response: AuthResponse = {statusCode: 409, message: 'USER_ALREADY_EXISTS'};
+      res.send(
+        response,
+      );
+    }
+  });
+}
