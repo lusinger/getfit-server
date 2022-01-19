aee60a705d96ba8b69c2f0e51d71aef8b844ee24
Lukas Singer <lukas.singer@outlook.com>
2022-01-19 10:10:06 +0100

    Implement getItems query function into getItems route

--- routes/get-items-route.ts
+++ routes/get-items-route.ts
@@ -1,19 +1,33 @@
 import {Express} from 'express';
 import { validateSessionToken } from '../modules/validation/jwt';
+import * as data from '../modules/database/data-queries';
+import { Item } from '../interfaces/item';
 
 export const getItems = (server: Express, url: string): Express => {
   return server.get(url, async(req, res) => {
-    console.log(req.query);
+    const search = req.query?.search ? req.query.search as string : undefined;
+    const start = req.query?.start ? parseInt(req.query.start as string) : undefined;
+    const end = req.query?.end ? parseInt(req.query.end as string) : undefined;
     const isValid = await validateSessionToken(req.cookies.LOGIN_TOKEN);
 
-    if(isValid !== null){
+    console.log(search, start, end);
 
+    if(isValid !== null){
+      const response = await data.getItems(search, start, end);
+      console.log(response);
+      if(response !== null){
+        res.send(
+          response,
+        )
+      }else{
+        res.send(
+          []
+        );
+      }
     }else{
-      res.send({
-      })
+      res.send(
+        []
+      );
     }
-    res.send({
-      id: 1
-    });
   });
 }
