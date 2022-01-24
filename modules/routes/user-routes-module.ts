7723b3c2a4d9b93fad311e63ab8de8c577e3f5c2
Lukas Singer <lukas.singer@outlook.com>
2022-01-24 19:36:04 +0100

    Refactor loadUserData route

--- modules/routes/user-routes-module.ts
+++ modules/routes/user-routes-module.ts
@@ -1,27 +1,37 @@
 import {Express} from 'express';
 import jwt from 'jsonwebtoken';
 import { AuthResponse } from '../../interfaces/auth-response';
+import { validateSessionToken } from '../validation/validation-module';
 
 import {query} from '../database/database-module';
 
 const loadUserData = (server: Express, url: string): Express => {
   return server.get(url, async(req, res) => {
     try {
-      const token = req.cookies.LOGIN_TOKEN;
-      const decoded = await jwt.decode(token) as any;
+      const token = req.cookies.SESSIONTOKEN;
+      const isTokenValid = await validateSessionToken(token);
 
-      const dbResponse = await query('SELECT id, username, mail, fullname, age, height, currentweight, targetweight, changeperweek, caloriegoal FROM users WHERE mail = $1', [decoded.mail]);
-
-      if(dbResponse.rowCount > 0){
-        const successResponse: AuthResponse = {statusCode: 200, message: 'USER_LOADED', payload: dbResponse.rows[0]};
-        res.send(
-          successResponse,
-        )
+      if(isTokenValid){
+        const {mail} = await jwt.decode(token) as any;
+        const dbResponse = await query('SELECT id, username, mail, fullname, age, height, currentweight, targetweight, changeperweek, gender, activityrating, caloriegoal, createdon  FROM users WHERE mail = $1', [mail]);
+        if(dbResponse.rowCount > 0){
+          res.send({
+            statusCode: 200,
+            message: 'user loaded',
+            payload: dbResponse.rows[0],
+          } as AuthResponse);
+        }else{
+          const errorResponse: AuthResponse = {statusCode: 404, message: 'FAILED_TO_LOAD_USER'};
+          res.send({
+            statusCode: 404, 
+            message: 'no user found'
+          } as AuthResponse);
+        }
       }else{
-        const errorResponse: AuthResponse = {statusCode: 404, message: 'FAILED_TO_LOAD_USER'};
-        res.send(
-          errorResponse,
-        )
+        res.send({
+          statusCode: 404,
+          message: 'token not valid'
+        } as AuthResponse);
       }
     } catch (err) {
       throw err;
