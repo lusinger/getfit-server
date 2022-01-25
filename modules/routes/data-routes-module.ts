b538139c7aa502a0ed3341d08d5d8fe545df9dc5
Lukas Singer <lukas.singer@outlook.com>
2022-01-25 09:06:21 +0100

    Create module handeling fetching modifying and deletion of data

--- modules/routes/data-routes-module.ts
+++ modules/routes/data-routes-module.ts
@@ -88,3 +88,33 @@ export const deleteEntryRoute = (server: Express, url: string): Express => {
     }
   });
 }
+
+const getItems = (server: Express, url: string): Express => {
+  return server.get(url, async(req, res) => {
+    const search = req.query?.search ? req.query.search as string : undefined;
+    const start = req.query?.start ? parseInt(req.query.start as string) : undefined;
+    const end = req.query?.end ? parseInt(req.query.end as string) : undefined;
+    const isValid = await validateSessionToken(req.cookies.LOGIN_TOKEN);
+
+    console.log(search, start, end);
+
+    if(isValid !== null){
+      const response = await data.getItems(search, start, end);
+      if(response !== null){
+        res.send(
+          response,
+        )
+      }else{
+        res.send(
+          []
+        );
+      }
+    }else{
+      res.send(
+        []
+      );
+    }
+  });
+}
+
+export {deleteEntry, addEntries, getEntry, getEntries, getItems};
